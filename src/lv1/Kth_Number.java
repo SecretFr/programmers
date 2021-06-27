6a8a8bfa560dad5aa8749b0c59aa5cf19c9a1aaa
SecretFr <linkclean@naver.com>
2021-06-27 15:25:55 +0900

    K번째 수 문제 풀이

---
+++ src/lv1/Kth_Number.java
@@ -0,0 +1,41 @@
+package lv1;
+import java.util.Arrays;
+
+class Solution{
+	public int[] solution(int[] array, int[][] commands) {
+		int[] answer = new int[commands.length];
+		for(int i=0; i<commands.length; i++) {
+			int[] temp = new int[commands[i][1]-commands[i][0]+1];
+			for(int j=0; j<temp.length; j++) {
+				temp[j] = array[j+commands[i][0]-1];
+			}
+			Arrays.sort(temp);
+			answer[i] = temp[commands[i][2]-1];
+		}
+		return answer;
+	}
+}
+
+public class Kth_Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = {1,5,2,6,3,7,4};
+		int[][] commands = {
+				{2,5,3},
+				{4,4,1},
+				{1,7,3}
+		};
+		int[] answer = {};
+		
+		Solution s = new Solution();
+		
+		answer = s.solution(array, commands);
+		
+		for(int i=0; i<answer.length; i++) {
+			System.out.print(answer[i]+" ");
+		}
+		
+	}
+
+}
