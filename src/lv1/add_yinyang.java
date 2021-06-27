e518b0556b63c921cd40a2f0e5037fe532649896
SecretFr <linkclean@naver.com>
2021-06-27 20:27:24 +0900

    add_yanyang

---
+++ src/lv1/add_yinyang.java
@@ -0,0 +1,33 @@
+package lv1;
+
+class Solution3{
+	public int solution(int[] absolutes, boolean[] signs) {
+		int answer =  0;
+		for(int i=0; i<absolutes.length; i++) {
+			if(signs[i] == true) {
+				absolutes[i] = Math.abs(absolutes[i]);
+			}
+			else if(signs[i] == false) {
+				absolutes[i] = -absolutes[i];
+			}
+			answer = answer + absolutes[i];
+		}
+		return answer;
+	}
+}
+
+public class add_yinyang {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution3 s = new Solution3();
+		int[] absolutes = {1,2,3};
+		boolean[] signs = {false, false, true};
+		int answer = 0;
+		
+		answer = s.solution(absolutes, signs);
+		
+		System.out.println(answer);
+	}
+
+}
