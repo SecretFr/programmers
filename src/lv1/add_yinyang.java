135043b5c72abfd2001af2862ffa36baba8076b0
SecretFr <linkclean@naver.com>
2021-06-27 23:58:59 +0900

    gitignore

--- src/lv1/add_yinyang.java
+++ src/lv1/add_yinyang.java
@@ -4,10 +4,7 @@ class Solution3{
 	public int solution(int[] absolutes, boolean[] signs) {
 		int answer =  0;
 		for(int i=0; i<absolutes.length; i++) {
-			if(signs[i] == true) {
-				absolutes[i] = Math.abs(absolutes[i]);
-			}
-			else if(signs[i] == false) {
+			if(signs[i] == false) {
 				absolutes[i] = -absolutes[i];
 			}
 			answer = answer + absolutes[i];
